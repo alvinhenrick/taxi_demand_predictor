d556a1da9f730b020f1cd642bfff7a3d668c6aac
Pau <plabartabajo@gmail.com>
2023-01-12 14:07:38 +0100

    Update inference.py

--- src/inference.py
+++ src/inference.py
@@ -58,7 +58,7 @@ def load_batch_of_features_from_store(
     # validate we are not missing data in the feature store
     location_ids = ts_data['pickup_location_id'].unique()
     assert len(ts_data) == n_features*len(location_ids), "Time-series data is not complete"
-
+    
     # sort data by location and time
     ts_data.sort_values(by=['pickup_location_id', 'pickup_hour'], inplace=True)
     print(f'{ts_data=}')
